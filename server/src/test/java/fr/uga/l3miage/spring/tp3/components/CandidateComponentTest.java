9db502ea3faf5dac569d60239e0dd6f2fb31ed64
l3miage-dumaslo <loic.dumas@etu.univ-grenoble-alpes.fr>
2024-04-06 12:49:56 +0200

    findAllByHasExtraTimeFalseAndBirthDateBefore fait

--- server/src/test/java/fr/uga/l3miage/spring/tp3/components/CandidateComponentTest.java
+++ server/src/test/java/fr/uga/l3miage/spring/tp3/components/CandidateComponentTest.java
@@ -1,11 +1,23 @@
 package fr.uga.l3miage.spring.tp3.components;
 
+import fr.uga.l3miage.spring.tp3.enums.TestCenterCode;
+import fr.uga.l3miage.spring.tp3.models.CandidateEntity;
+import fr.uga.l3miage.spring.tp3.models.TestCenterEntity;
 import fr.uga.l3miage.spring.tp3.repositories.CandidateRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
 
 @AutoConfigureTestDatabase
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
@@ -16,16 +28,36 @@ public class CandidateComponentTest {
     private CandidateRepository candidateRepository;
 
     @Test
-    void getCandidateAverage(){
+    void getCandidateAverageTest(){
         //Given
-        PlaylistEntity playlistEntity = PlaylistEntity.builder()
-                .songEntities(Set.of())
-                .description("test")
+
+        TestCenterEntity testCenter = TestCenterEntity
+                .builder()
+                .code(TestCenterCode.GRE)
+                .university("UGA")
+                .city("GRENOBLE")
+                .candidateEntities(new HashSet<>())
+                .examinerEntities(new HashSet<>())
                 .build();
 
-        when(playlistRepository.findById(anyString())).thenReturn(Optional.of(playlistEntity));
 
-        // when - then
-        assertDoesNotThrow(()->playlistComponent.getPlaylist("test"));
+        CandidateEntity candidateEntity = CandidateEntity
+                .builder()
+                .firstname("Teddie")
+                .lastname("GREGOIRE")
+                .email("teddie.gregoire@hotmail.com")
+                .phoneNumber("07871656")
+                .birthDate(LocalDate.now())
+                .hasExtraTime(false)
+                .candidateEvaluationGridEntities(new HashSet<>())
+                .testCenterEntity(testCenter)
+                .build();
+
+
+
+        when(candidateRepository.findById(anyLong())).thenReturn(Optional.of(candidateEntity));
+
+            // when - then
+        assertDoesNotThrow(()->candidateComponent.getCandidatById(anyLong()));
     }
 }
