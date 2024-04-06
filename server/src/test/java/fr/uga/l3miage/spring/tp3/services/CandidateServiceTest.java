f2d74104cf0e333a7195f77f58ff36b2f17df758
l3miage-dumaslo <loic.dumas@etu.univ-grenoble-alpes.fr>
2024-04-06 14:47:30 +0200

    findAllByHasExtraTimeFalseAndBirthDateBefore fait

---
+++ server/src/test/java/fr/uga/l3miage/spring/tp3/services/CandidateServiceTest.java
@@ -0,0 +1,82 @@
+package fr.uga.l3miage.spring.tp3.services;
+
+import fr.uga.l3miage.spring.tp3.components.CandidateComponent;
+import fr.uga.l3miage.spring.tp3.enums.TestCenterCode;
+import fr.uga.l3miage.spring.tp3.mappers.ExamMapper;
+import fr.uga.l3miage.spring.tp3.models.*;
+import fr.uga.l3miage.spring.tp3.repositories.CandidateRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hibernate.validator.internal.util.Contracts.assertTrue;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+
+@AutoConfigureTestDatabase
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+class CandidateServiceTest {
+    @Autowired
+    private CandidateComponent candidateComponent;
+    @MockBean
+    private CandidateService candidateService;
+    @MockBean
+    private CandidateRepository candidateRepository;
+
+
+    @Test
+    void getCandidateAverageTest(){
+        //given
+
+        TestCenterEntity testCenter = TestCenterEntity
+                .builder()
+                .code(TestCenterCode.GRE)
+                .university("UGA")
+                .city("GRENOBLE")
+                .candidateEntities(new HashSet<>())
+                .examinerEntities(new HashSet<>())
+                .build();
+
+        CandidateEntity candidateEnti = CandidateEntity
+                .builder()
+                .firstname("Teddie")
+                .lastname("GREGOIRE")
+                .email("teddie.gregoire@hotmail.com")
+                .phoneNumber("07871656")
+                .birthDate(LocalDate.now())
+                .hasExtraTime(false)
+                .candidateEvaluationGridEntities(new HashSet<>())
+                .testCenterEntity(testCenter)
+                .build();
+
+        CandidateEvaluationGridEntity candidateEvaluationGridEntity = CandidateEvaluationGridEntity
+                .builder()
+                .sheetNumber(12L)
+                .grade(15)
+                .submissionDate(LocalDateTime.now())
+                .candidateEntity(candidateEnti)
+                .build();
+
+
+        when(candidateRepository.findById(anyLong())).thenReturn(Optional.of(candidateEnti));
+
+        // When
+        Double optionalCandidateEntity = candidateService.getCandidateAverage(anyLong());
+
+        // Then
+        assertThat(optionalCandidateEntity).isEqualTo(15);
+    }
+}
+
